e5195b123a2f5988449d3d1d276f509e6087b87b
PetrP99 <petr2@bk.ru>
2023-09-01 10:30:36 +0300

    task string

--- Git.java
+++ Git.java
@@ -1,7 +1,20 @@
 public class Git {
 
     public static void main(String[] args) {
-        String name;
-        int age;
+        String value = "Akkvf fsss92DFf ffEgas";
+        System.out.println(answerUpper(value));
+
+    }
+    public static String answerUpper (String value) {
+        String result = value.replace(" ", "").toUpperCase();
+        StringBuilder sb = new StringBuilder(result);
+        for (int i = 1; i < sb.length(); i++) {
+            if (sb.charAt(i) == sb.charAt(i-1)) {
+                sb.deleteCharAt(i);
+                i--;
+
+            }
+        }
+        return sb.toString();
     }
 }
