478993176c15dbcc3fba22f1b7759042d4296960
petrp99 <petr2@bk.ru>
2023-09-04 16:07:56 +0300

    chanched task1

--- AlgoTask1.java
+++ AlgoTask1.java
@@ -12,54 +12,50 @@ public class AlgoTask1 {
         int day = 0;
         int month = 0;
         int year = 0;
-
         Scanner sc = new Scanner(System.in);
+
+
         System.out.print("Введи число дня от 1 до 31: ");
         while (day == 0) {
             int temp = sc.nextInt();
-            if (temp < 0 || temp > 31) System.err.print("Неверно!, введи число дня от 1 до 31: ");
-            else {
-                day = temp;
-                System.out.println("day " + day);
-            }
+            if (temp <= 0 || temp > 31) System.err.print("Неверно!, введи число дня от 1 до 31: ");
+            else day = temp;
         }
+
         System.out.print("Введи число месяца от 1 до 12: ");
         while (month == 0) {
             int temp = sc.nextInt();
-            if (temp < 0 || temp > 12) System.err.print("Неверно!, введи число месяца от 1 до 12:  ");
-            else {
-                month = temp;
-                System.out.println("month " + month);
-            }
+            if (temp <= 0 || temp > 12) System.err.print("Неверно!, введи число месяца от 1 до 12:  ");
+            else month = temp;
         }
-
 
         System.out.print("Введи год от 1 до 2023: ");
         while (year == 0) {
             int temp = sc.nextInt();
             if (temp < 1 || temp > 2023) System.out.print("Введи год от 1 до 2023: ");
-            else {
-                year = temp;
-                System.out.println("year " + year);
-            }
+            else year = temp;
         }
-        System.out.printf("Введенная дата: %d.%d.%d",day,month,year);
-        System.out.println();
-        System.out.print("Дата следующего дня: " + result(day,month,year));
-
+        dataPrint(day, month, year);
     }
-    public static String result (int day, int month, int year) {
+     public static void dataPrint(int day, int month, int year) {
+         System.out.printf( "\nВведенная дата: %d.%d.%d", day, month, year);
+         System.out.println();
+         System.out.print("Дата следующего дня: " + nextData(day, month, year));
+     }
+
+    public static String nextData(int day, int month, int year) {
         if (day == 31 && month == 12) {
             day = 1;
             month = 1;
             year++;
-                    }
-        else {
-            day++;
-            month++;
-        }
+        } else if (day == 31) {
+            day = 1;
+            month ++;
+        } else day++;
+
         String result = day + "." + month + "." + year;
         return result;
+
     }
 }
 
