3b34957db2aa5e0c5778f951ac7b88a02d0836f9
petrP99 <143410832+petrP99@users.noreply.github.com>
2023-09-15 10:46:13 +0300

    Update Task3.java

--- Task3.java
+++ Task3.java
@@ -10,17 +10,14 @@
 
 public class Task3 {
     public static void main(String[] args) {
-        double s1 = square1(-2,5);
-        double s2 = square2(51,2);
+        double s1 = square(-2,5);
+        double s2 = square(51,2);
         System.out.println("Площадь первого треугольника: " + s1);
         System.out.println("Площадь второго треугольника: " + s2);
         equalsSquare(s1,s2);
     }
-    private static double square1 (int a, int b) {
+    private static double square (int a, int b) {
         return (double) (a * b) /2;
-    }
-    private static double square2 (int a2, int b2) {
-        return (double) (a2 * b2) /2;
     }
     private static void equalsSquare (double s1, double s2) {
         if (s1 == s2) System.out.println("Первый треугольник равен второму");
